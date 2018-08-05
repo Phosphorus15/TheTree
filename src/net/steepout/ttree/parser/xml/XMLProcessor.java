97f731d4992e003cdfae4973f488fd939195bd51
Phosphorus15 <steepout@qq.com>
2018-08-06 00:42:18 +0800

    - Trying hard to be compatible with XML

--- src/net/steepout/ttree/parser/xml/XMLProcessor.java
+++ src/net/steepout/ttree/parser/xml/XMLProcessor.java
@@ -1,11 +1,15 @@
 package net.steepout.ttree.parser.xml;
 
+import net.steepout.ttree.NodeType;
 import net.steepout.ttree.TreeManager;
+import net.steepout.ttree.TreeNode;
 import net.steepout.ttree.TreeRoot;
 import net.steepout.ttree.parser.TreeProcessor;
 
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 public class XMLProcessor extends TreeProcessor {
 
@@ -20,7 +24,24 @@ public class XMLProcessor extends TreeProcessor {
 
     @Override
     public ByteBuffer serialize(TreeRoot root) {
-        return null;
+        return ByteBuffer.wrap(toXML(root, 1).getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String toXML(TreeNode node, int level) {
+        StringBuilder tabs = new StringBuilder();
+        for (int i = 0; i < level; i++) tabs.append('\t');
+        if (node.getName() == null && node.getType() == NodeType.TYPE_STRING) return node.getValue().toString() + "\n";
+        if (node.getValue() == null) {
+            return tabs.append("<").append(safeName(node.getName())).append("/>").toString();
+        } else if (node.getType() == NodeType.TYPE_DATA_LIST) {
+
+        } else if (node.getType() == NodeType.TYPE_HEADER) {
+            //FIXME a lot of damn compatibility-related problems
+        } else return tabs.append("<");
+    }
+
+    private String safeName(String name) {
+        return name == null ? UUID.randomUUID().toString() : name;
     }
 
     @Override
---
+++ tests/net/steepout/ttree/parser/xml/XMLProcessorTest.java
@@ -0,0 +1,17 @@
+package net.steepout.ttree.parser.xml;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class XMLProcessorTest {
+
+    @Test
+    void serialize() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void parse() {
+    }
+}
